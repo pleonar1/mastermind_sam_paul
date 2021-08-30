ead8a8f38ab7a4b2a122377d732d65abb8421918
Paul Leonard <paulpaulleonard@gmail.com>
2021-08-30 16:30:01 -0400

    Add game class

---
+++ lib/game.rb
@@ -0,0 +1,15 @@
+class Game
+  def initialize
+
+  end
+
+  def start(input)
+    ##if the input is play, start the Game
+    ## If the input is quit, quit the game
+    ## IF the input is intructions, print instructions
+  end
+
+  
+
+
+end
