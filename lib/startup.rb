fab14829923ad497d3e3ee836ce904f5a41e8e7e
Sam Devine <samuel.will.devine@gmail.com>
2021-08-30 15:31:09 -0600

    Refactor Game class and rename to Startup

--- lib/startup.rb
+++ lib/startup.rb
@@ -1,32 +1,35 @@
-class Game
-  def initialize
+class Startup
 
+  def initialize
   end
 
-  def start
+  def intro
     puts "Welcome to MASTERMIND\n"
     puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
     print ">"
     input = $stdin.gets.chomp
     if input.downcase == "p"
-      #play method
+      play
     elsif input.downcase == "i"
-      instruction
+      instructions
     elsif input.downcase == "q"
-      #quit method?
+      quit
     else
-      start
+      intro # is this bad?
     end
   end
 
   def play
+    game = Game.new
+    game.start
   end
 
-  def instruction
+  def instructions
     p "abc"
   end
 
   def quit
+    p "Goodbye"
   end
 
 end
