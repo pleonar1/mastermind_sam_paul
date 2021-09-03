1faea73d4a61dad4d6851dfcb3e9fcbe8c2add6b
Paul Leonard <paulpaulleonard@gmail.com>
2021-09-02 20:41:20 -0400

    Add timer and end game

--- lib/game.rb
+++ lib/game.rb
@@ -21,10 +21,14 @@ class Game
     print ">"
 
     turn_flow
+
   end
 
   def turn_flow
+
+    start_time = Time.now
     correct_positions = 0
+    turn_counter = 0
 
     until correct_positions == 4
 
@@ -42,13 +46,24 @@ class Game
         evaluator = Evaluator.new(input, @code)
         correct_colors = evaluator.compare_colors(input)
         correct_positions = evaluator.compare_positions(input)
+        turn_counter += 1
+        if winner?(correct_positions)
+          end_time = Time.now
+          elapsed_time = end_time - start_time
+          elapsed_time_minutes = end_time.min - start_time.min
 
-        puts "You have #{correct_colors} correct colors and #{correct_positions} are in the correct positions"
+          puts "Congratulations! You guessed the sequence #{@code} in #{turn_counter} guesses over #{elapsed_time_minutes} minutes and #{(elapsed_time % 60).to_i} seconds"
+        else
+          puts "You have #{correct_colors} correct colors and #{correct_positions} are in the correct positions" ## can't get this to not print when we win yet
+        end
       end
     end
   end
 
-
   def quit
+  end
+
+  def winner?(correct_positions)
+    correct_positions == 4
   end
 end
