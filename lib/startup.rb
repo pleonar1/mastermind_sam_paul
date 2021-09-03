bcad4b5e4cac996c20ba71107e1806f420cfcc7e
Paul Leonard <paulpaulleonard@gmail.com>
2021-09-02 21:23:59 -0400

    Add instruction method

--- lib/startup.rb
+++ lib/startup.rb
@@ -28,7 +28,19 @@ class Startup
   end
 
   def instructions
-    p "abc"
+    p "1) When a player starts the game, a random sequence of the colors Red,"
+    p "Blue, Green, or Yellow will be generated as a secret code of 4 characters"
+    p "2) For higher level difficulty Intermediate, Purple is included and the"
+    p "sequence is 5 characters"
+    p "3) For higher level difficulty Advanced Purple and Orange are included"
+    "and the sequence is 6 characters"
+    p "4) The player will try to guess the secret code by inputing the"
+    p "corresponding color's keys such as 'rybg'"
+    p "5) Red = r, Blue = b, Yellow = y, Green = g, Purple = p, Orange = o"
+    p "6) The player will be told after each guess how many colors they guessed"
+    "correctly and how many colors they guessed in the correct position"
+    p "7) The game ends when the player correctly guesses the secret code"
+    intro
   end
 
   def quit
