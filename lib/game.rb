d558310e20efe82f1f77d3d617e2dce453fce366
Sam Devine <samuel.will.devine@gmail.com>
2021-09-04 13:00:54 -0600

    Move timer to new method in game class

--- lib/game.rb
+++ lib/game.rb
@@ -26,9 +26,18 @@ class Game
 
   end
 
-  def turn_flow
+  def valid_input?(input)
+    if input.length < 4
+      puts "Too short, guess again"
+      false
+    elsif input.length > 4
+      puts "Too long, guess again"
+      false
+    end
+  end
 
-    start_time = Time.now
+  def turn_flow
+    time = start_time
     correct_positions = 0
     turn_counter = 0
 
@@ -41,23 +50,22 @@ class Game
         break
       elsif input.downcase == "c" || input.downcase == "cheat"
         puts @code
-      elsif input.length < 4
-        puts "Too short, guess again"
-      elsif input.length > 4
-        puts "Too long, guess again"
+      elsif valid_input?(input) == false
       else
         evaluator = Evaluator.new(input, @code)
         correct_colors = evaluator.compare_colors(input)
         correct_positions = evaluator.compare_positions(input)
         turn_counter += 1
         if winner?(correct_positions)
-          end_time = Time.now
-          elapsed_time = end_time - start_time
-          elapsed_time_minutes = end_time.min - start_time.min
-
-          puts "Congratulations! You guessed the sequence #{@code} in #{turn_counter} guesses over #{elapsed_time_minutes} minutes and #{(elapsed_time % 60).to_i} seconds"
+          game_end(time, turn_counter)
+          break
         else
-          puts "You have #{correct_colors} correct colors and #{correct_positions} are in the correct positions" ## can't get this to not print when we win yet
+          puts "You have #{correct_colors} correct colors and #{correct_positions} are in the correct positions"
+          if turn_counter != 1
+            puts "You've taken #{turn_counter} guesses."
+          else
+            puts "You've taken 1 guess."
+          end
         end
       end
     end
@@ -78,5 +86,27 @@ class Game
 
   def winner?(correct_positions)
     correct_positions == 4
+  end
+
+  def start_time
+    Time.now
+  end
+
+  def elapsed_time(time)
+    mins_secs = []
+
+    elapsed_time = Time.now - time
+
+    mins_secs << (elapsed_time).to_i / 60 # minutes (index 0)
+    mins_secs << (elapsed_time % 60).to_i # seconds (index 1)
+
+    mins_secs
+  end
+
+  def game_end(time, turn_counter)
+    turn_counter = turn_counter
+    final_time = elapsed_time(time)
+
+    puts "Congratulations! You guessed the sequence #{@code} in #{turn_counter} guesses over #{final_time[0]} minutes and #{final_time[1]} seconds"
   end
 end
