dcd1725eae7dfd7a873327ca326a31c4eaa93bec
Paul Leonard <paulpaulleonard@gmail.com>
2021-09-07 13:15:55 -0400

    Finish evaluator tests

--- spec/evaluator_spec.rb
+++ spec/evaluator_spec.rb
@@ -2,33 +2,34 @@ require './lib/evaluator.rb'
 
 RSpec.describe do
   it 'exists' do
-    guess = ['R', 'R', 'B', 'B']
     code = ['R', 'R', 'R', 'R']
-    evaluator = Evaluator.new(guess, code)
-
+    evaluator = Evaluator.new(code)
 
     expect(evaluator).to be_a Evaluator
   end
 
-  it "can compare positons" do### might need input here
-    guess = ['R', 'R', 'B', 'B']
-    code = ['R', 'R', 'R', 'R']
-    input = 'RRBB'
-    evaluator = Evaluator.new(guess, code)
-    evaluation = evaluator.compare_positions(input)
+  it "can compare positons" do
+    sequence = ['R', 'R', 'R', 'R']
+    input_1 = 'RRBB'
+    input_2 = 'yygr'
+    evaluator = Evaluator.new(sequence)
 
-    expect(evaluation).to eq(2)
+    evaluation_1 = evaluator.compare_positions(input_1)
+    evaluation_2 = evaluator.compare_positions(input_2)
+
+    expect(evaluation_1).to eq(2)
+    expect(evaluation_2).to eq(1)
   end
 
   it "can compare colors" do
-    guess = ['R', 'R', 'B', 'B']
     sequence = ['R', 'R', 'R', 'R']
-    input = 'RRBB'
-    evaluator = Evaluator.new(guess, sequence)
-    evaluation = evaluator.compare_colors(input)
+    input_1 = 'RRBB'
+    input_2 = 'rrbr'
+    evaluator = Evaluator.new(sequence)
+    evaluation_1 = evaluator.compare_colors(input_1)
+    evaluation_2 = evaluator.compare_colors(input_2)
 
-
-
-    expect(evaluation).to eq(2)
+    expect(evaluation_1).to eq(2)
+    expect(evaluation_2).to eq(3)
   end
 end
