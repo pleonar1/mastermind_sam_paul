e336a21aacc486e14338ebbb3e7f5a522ef8a2e9
Sam Devine <samuel.will.devine@gmail.com>
2021-09-07 19:21:41 -0600

    refactor: Remove quit and instructions methods, use stub to test #start_game

--- lib/main_menu.rb
+++ lib/main_menu.rb
@@ -12,29 +12,24 @@ class MainMenu
     input = $stdin.gets.chomp
 
     if input.downcase == "p" || input.downcase == "play"
-      play
+      game = create_game
+      start_game(game)
     elsif input.downcase == "i" || input.downcase == "instructions"
       printer.instructions
       menu
     elsif input.downcase == "q" || input.downcase == "quit"
-      quit
+      printer.quit_menu
     else
       puts "Input not understood."
       menu
     end
   end
 
-  def play
-    game = Game.new
+  def create_game
+    Game.new
+  end
+
+  def start_game(game)
     game.start
-  end
-
-  def instructions
-    printer.instructions
-    menu
-  end
-
-  def quit
-    p "Have a nice day!"
   end
 end
